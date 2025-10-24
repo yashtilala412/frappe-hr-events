233fe03aedd334b86711e4857207aa973ae7a76d
yashtilala412 <yashtilala412@gmail.com>
2025-10-24 05:32:39 +0000

    feat:user meta doctype added successfully

---
+++ hr_events/hr_events/doctype/user_meta/user_meta.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, yashtilala and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("User Meta", {
+// 	refresh(frm) {
+// 		// Your code here
+// 	},
+// });
